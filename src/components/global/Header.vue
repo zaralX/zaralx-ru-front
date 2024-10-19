f00ce61db4659515aa05cfb219f515f6f1b29499
zaralX <61970128+zaralX@users.noreply.github.com>
2024-10-19 21:29:58 +0800

    mobile menu

--- src/components/global/Header.vue
+++ src/components/global/Header.vue
@@ -8,6 +8,8 @@ const links = [
   { name: "_about", to: "/about" }
 ];
 
+const mobileMenu = ref(false);
+
 // Позиция и ширина для линии
 const activeLinkIndex = ref(0);
 const linkRefs = ref([]); // Ссылки на DOM-элементы
@@ -50,8 +52,23 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div class="h-16 border-b-2 border-border-main-light dark:border-border-main-dark flex justify-center items-center text-border-inside-light dark:text-border-inside-dark font-[450] text-lg">
-    <div class="px-4 md:min-w-64 h-full flex items-center text-center">zaralx-website</div>
+  <div class="fixed w-screen h-screen top-0 left-0 z-20">
+    <div :class="mobileMenu ? 'top-0' : '-top-full'" class="absolute w-screen h-screen bg-bg-second-light dark:bg-bg-second-dark transition-all duration-200 ease-out pt-16">
+      <div class="text-2xl flex flex-col text-border-inside-light dark:text-border-inside-dark">
+        <router-link
+            v-for="link in links"
+            :to="link.to"
+            class="flex items-center justify-center border-border-third-light dark:border-border-third-dark transition-all duration-300 border-b-2 border-border-main-light dark:border-border-main-dark py-2"
+            :class="route.fullPath === link.to ? '!text-amber-500' : ''"
+            @click="mobileMenu = false"
+        >
+          {{ link.name }}
+        </router-link>
+      </div>
+    </div>
+  </div>
+  <div class="h-16 border-b-2 border-border-main-light dark:border-border-main-dark flex justify-center items-center text-border-inside-light dark:text-border-inside-dark font-[450] text-lg z-30 bg-bg-main-light dark:bg-bg-main-dark">
+    <div class="px-8 md:min-w-64 h-full flex items-center text-center">zaralx-website</div>
 
     <nav class="relative grow border-x-2 border-border-main-light dark:border-border-main-dark h-full lg:grid grid-cols-5 hidden">
       <!-- Линия под активной ссылкой -->
@@ -74,6 +91,10 @@ onUnmounted(() => {
       </router-link>
     </nav>
 
-    <div class="px-4 md:px-1 md:min-w-64 h-full flex items-center justify-center">_contact-me</div>
+    <div class="px-4 md:min-w-64 h-full hidden md:flex items-center justify-center">_contact-me</div>
+    <div @click="mobileMenu = !mobileMenu" class="px-8 grow h-full flex md:hidden items-center justify-end active:text-amber-500 duration-100 transition-all">
+      <i v-if="!mobileMenu" class="pi pi-bars"></i>
+      <i v-if="mobileMenu" class="pi pi-times"></i>
+    </div>
   </div>
 </template>
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -6,8 +6,8 @@
 <!--  <div-->
 <!--      class="absolute inset-0 h-full w-full bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:32px_32px]"-->
 <!--  ></div>-->
-<div class="text-7xl">
-  zaralX Developer Website
+<div class="text-5xl">
+  Hello
 </div>
 </template>
 
