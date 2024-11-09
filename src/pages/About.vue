b9392ce40ca50548ef88ceaa0c020e8c5046cd27
zaralX <61970128+zaralX@users.noreply.github.com>
2024-11-09 16:58:28 +0800

    _about -> _projects

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -9,23 +9,7 @@
 <div class="text-lg md:px-32 xl:px-64 py-8">
   <h1>Обо мне</h1>
   <p>Я – программист-самоучка с 4-летним опытом разработки. За это время я освоил широкий спектр языков программирования и фреймворков, применяя их на практике в создании различных проектов. Мой опыт включает как работу над коммерческими проектами, так и разработку собственных решений, что позволило мне глубже погрузиться в мир программирования и постоянно совершенствовать свои навыки.</p>
-  <h1 class="mt-4">Проекты</h1>
-  <p>Список больших и просто интересных проектов (это не все проекты):</p>
-  <ul class="list-disc mt-2 text-xl">
-    <li><a href="https://spmk.zaralx.ru" target="_blank" class="text-orange-500 underline">SPm Combat</a> <br> <span class="text-sm opacity-75">Игра пародирующая популярный в 2024 году Hamster Combat, но со своими фичами. Работающая на <a
-        href="https://spworlds.ru" target="_blank" class="underline">СПм</a></span></li>
-    <li><a href="https://t.me/zWork1" target="_blank" class="text-red-500 underline">
-      Столбы [Закрытый доступ. Купить: @zWork1]</a> <br> <span class="text-sm opacity-75">Minecraft Bukkit плагин добавляющий игру Столбы, ваша задача - остаться последним, получая случайные предметы. [Плагин имеет большой функционал: Карты, поддержка игр от 2 до 100 игроков, Достижения и т.д.]</span></li>
-    <li><a href="https://t.me/zWork1" target="_blank" class="text-red-500 underline">
-      Мосты [Закрытый доступ. Купить: @zWork1]</a> <br> <span class="text-sm opacity-75">Minecraft Bukkit плагин добавляющий игру Мосты, ваша задача - вместе с командой как можно быстрее собрать ресурсы для строителя, вы можете телепортироваться на базы противников и воровать у них ресурсы.</span></li>
-    <li><a href="https://github.com/zaralX/GriefMod" target="_blank" class="text-emerald-500 underline">Grief Mod <span class="italic">[Source]</span></a> <br> <span class="text-sm opacity-75">Minecraft Bukkit плагин который предоставляет ограничения для новых игроков (Например чтобы пока игрок не проведёт на сервере 24ч. он не сможет никого ударить)</span></li>
-    <li><a href="https://github.com/zaralX/InfinityButtonSimulator" target="_blank" class="text-pink-500 underline">InfinityButtonSimulator <span class="italic">[Source]</span></a> <br> <span class="text-sm opacity-75">Minecraft Bukkit плагин с простой игрой симулятором и топом. Создано за 24ч.</span></li>
-    <li><a href="https://www.youtube.com/watch?v=5Qjz4l0CZho" target="_blank" class="text-amber-500 underline">Cristalix Dungeons</a> <br> <span class="text-sm opacity-75">Участвовал в разработке игрового режима на сервере <a
-        href="https://cristalix.gg" target="_blank" class="underline">cristalix.gg</a></span></li>
-    <li><a href="https://www.youtube.com/watch?v=RVah56XIITM" target="_blank" class="text-blue-500 underline">FreshMarket</a> <br> <span class="text-sm opacity-75">Маркетплейс в игре Minecraft который успешно работал 8 месяцев</span></li>
-    <li><a href="https://img.zaralx.ru" target="_blank" class="text-purple-500 underline">Image Service</a> <br> <span class="text-sm opacity-75">API которое предоставляет minecraft иконки предметов, а так же майнкрафт скины</span></li>
-    <li><a href="" target="_blank" class="text-blue-400 line-through">Minecast Launcher</a> <br> <span class="text-sm opacity-75">Лаунчер который я создавал для своего майнкрафт сервера (Сейчас проект закрыт)</span></li>
-  </ul>
+  <h1 class="mt-4 text-base opacity-75 text-sky-300">// TODO: Добавить скиллы, и сделать страницу интересней</h1>
 </div>
 </template>
 
--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,34 +1,174 @@
 <script setup>
 import {ref} from "vue";
 import Checkbox from "../components/global/Checkbox.vue";
-import {tag} from "postcss-selector-parser";
 
 const tags = ref([
   {
-    key: "t1",
-    name: "Test 1",
-    icon: "/logos/html.svg",
+    key: "minecraft",
+    name: "Minecraft",
+    icon: "/logos/minecraft-cube.svg",
     selected: false
   },
   {
-    key: "t2",
-    name: "Test 2",
-    icon: "/logos/html.svg",
+    key: "website",
+    name: "Website",
+    icon: "/logos/globe.svg",
+    selected: false
+  },
+  {
+    key: "application",
+    name: "Application",
+    icon: "/logos/browser.svg",
+    selected: false
+  },
+  {
+    key: "source",
+    name: "Source Code",
+    icon: "/logos/source-code.svg",
     selected: false
   }
 ])
 
 const projects = [
   {
-    name: "Empty Project",
-    description: "Проекты скоро будут перенесен из _about",
-    type: "_hz_cho_eto",
-    image: "/filters/3.png",
-    tags: ["t1"],
+    name: "SPm Combat",
+    description: "Игра пародирующая популярный в 2024 году Hamster Combat, но со своими фичами. Работающая на СПм.",
+    type: "_hamster_criminal",
+    image: "/fillers/3.png",
+    tags: ["website", "minecraft"],
+    date: "2024-08-19",
+    links: [
+      {
+        type: "website",
+        link: "https://spmk.zaralx.ru/"
+      }
+    ]
+  },
+  {
+    name: "Pillars",
+    description: "Minecraft Bukkit плагин добавляющий игру Столбы, ваша задача - остаться последним, получая случайные предметы.",
+    type: "_best_pillars",
+    image: "/fillers/2.png",
+    tags: ["minecraft"],
+    date: "2024-03-16",
+    links: [
+      {
+        type: "buy-tg",
+        link: "https://zWork1.t.me/"
+      },
+      {
+        type: "youtube",
+        link: "https://www.youtube.com/watch?v=nvlq8B7DDHk"
+      }
+    ]
+  },
+  {
+    name: "BridgeBuilders",
+    description: "Minecraft Bukkit плагин добавляющий игру Мосты, ваша задача - вместе с командой как можно быстрее собрать ресурсы для строителя, вы можете телепортироваться на базы противников и воровать у них ресурсы.",
+    type: "_new_bridges",
+    image: "/fillers/2.png",
+    tags: ["minecraft"],
+    date: "2024-06-23",
+    links: [
+      {
+        type: "buy-tg",
+        link: "https://zWork1.t.me/"
+      },
+      {
+        type: "youtube",
+        link: "https://www.youtube.com/shorts/-WimqjhG9G0"
+      }
+    ]
+  },
+  {
+    name: "Grief Mod",
+    description: "Minecraft Bukkit плагин который предоставляет ограничения для новых игроков (Например чтобы пока игрок не проведёт на сервере 24ч. он не сможет никого ударить)",
+    type: "_anti_grief",
+    image: "/fillers/1.png",
+    tags: ["minecraft", "source"],
+    date: "2024-04-11",
+    links: [
+      {
+        type: "source-github",
+        link: "https://github.com/zaralX/GriefMod"
+      }
+    ]
+  },
+  {
+    name: "InfinityButtonSimulator",
+    description: "Minecraft Bukkit плагин с простой игрой симулятором и топом. Создано за 24ч.",
+    type: "_infinity_afk",
+    image: "/fillers/1.png",
+    tags: ["minecraft", "source"],
+    date: "2024-04-29",
+    links: [
+      {
+        type: "source-github",
+        link: "https://github.com/zaralX/InfinityButtonSimulator"
+      }
+    ]
+  },
+  {
+    name: "Cristalix Dungeons",
+    description: "Разработка игрового режима в команде на проекте cristalix.gg",
+    type: "_cristalix_dev",
+    image: "/fillers/3.png",
+    tags: ["minecraft"],
+    date: "2024-04-06",
     links: [
       {
         type: "youtube",
-        link: "#"
+        link: "https://www.youtube.com/watch?v=5Qjz4l0CZho"
+      },
+      {
+        type: "website",
+        link: "https://cristalix.gg"
+      }
+    ]
+  },
+  {
+    name: "FreshMarket",
+    description: "Маркетплейс в игре Minecraft который предоставляет продажу и покупку товаров.",
+    type: "_marketplace",
+    image: "/fillers/3.png",
+    tags: ["minecraft", "website"],
+    date: "2023-07-28",
+    links: [
+      {
+        type: "youtube",
+        link: "https://www.youtube.com/watch?v=RVah56XIITM"
+      },
+      {
+        type: "website",
+        link: "https://spx.zaralx.ru"
+      }
+    ]
+  },
+  {
+    name: "Image Service",
+    description: "API которое предоставляет minecraft иконки предметов, а так же майнкрафт скины",
+    type: "_minecraft_icons",
+    image: "/fillers/2.png",
+    tags: ["minecraft", "website"],
+    date: "2023-11-01",
+    links: [
+      {
+        type: "website",
+        link: "https://img.zaralx.ru"
+      }
+    ]
+  },
+  {
+    name: "Minecast Launcher",
+    description: "Лаунчер для майнкрафт сервера. (Сейчас проект закрыт)",
+    type: "_minecraft_launcher",
+    image: "/fillers/1.png",
+    tags: ["minecraft", "application"],
+    date: "2024-06-12",
+    links: [
+      {
+        type: "website",
+        link: "https://github.com/zaralX/minecast-launcher-updates/releases/latest"
       }
     ]
   }
@@ -58,6 +198,13 @@ const updateShowingProjects = () => {
   }
 }
 
+const tagsTitle = () => {
+  const filteredTags = tags.value.filter(tag => tag.selected);
+  if (filteredTags.length === 0) return "all";
+  return filteredTags.map(tag => tag.name).join('; ');
+}
+
+// TODO: Mobile adaptation
 </script>
 
 <template>
@@ -80,7 +227,11 @@ const updateShowingProjects = () => {
 
   <div class="flex-1 border-l-2 border-border-main-dark flex flex-col">
     <div class="h-10 flex items-center gap-2 border-b-2 border-border-main-light dark:border-border-main-dark text-border-inside-more-light dark:text-border-inside-more-dark">
-      <p class="pl-4 pr-4 max-w-64 overflow-ellipsis truncate">{{tags.map(tag => tag.name).join('; ')}}</p>
+      <p class="pl-4 pr-4 max-w-64 overflow-ellipsis truncate">{{ tagsTitle() }}</p>
+      <div class="h-full border-r-2 border-border-main-light dark:border-border-main-dark flex justify-center items-center pr-4">
+        <i class="pi pi-times text-xs"></i>
+      </div>
+      <p class="pl-4 pr-4 max-w-96 overflow-ellipsis truncate">ЗДЕСЬ НЕ ПОЛНЫЙ СПИСОК ПРОЕКТОВ</p>
       <div class="h-full border-r-2 border-border-main-light dark:border-border-main-dark flex justify-center items-center pr-4">
         <i class="pi pi-times text-xs"></i>
       </div>
@@ -99,19 +250,28 @@ const updateShowingProjects = () => {
               </div>
             </div>
           </div>
-          <div class="w-full h-32 bg-cover rounded-t-xl border-b-[1px] border-slate-700/[.4]" style="background-image: url('/fillers/1.png')"></div>
-          <div class="p-4 text-slate-400 font-medium flex flex-col flex-grow h-32">
-            <div class="flex-1">{{project.description}}</div>
+          <div class="w-full h-32 bg-cover rounded-t-xl border-b-[1px] border-slate-700/[.4]" :style="'background-image: url('+project.image+')'"></div>
+          <div class="p-4 text-slate-400 font-medium flex flex-col flex-grow h-40">
+            <div class="flex-1 line-clamp-4">{{project.description}}</div>
             <div class="flex gap-2">
               <a v-for="link in project.links" :href="link.link" class="text-white py-1 px-2 rounded-lg flex justify-center items-center gap-1"
               :class="
-              link.type === 'youtube' ? 'bg-red-500 hover:bg-red-600' : '?'
+              link.type === 'youtube' ? 'bg-red-500 hover:bg-red-600' :
+                link.type === 'website' ? 'bg-blue-600 hover:bg-blue-700' :
+                  link.type === 'buy-tg' ? 'bg-sky-600 hover:bg-sky-700' :
+                    link.type === 'source-github' ? 'bg-slate-600 hover:bg-slate-700' : '?'
 ">
                 <img class="w-6 h-6" :src="
-                link.type === 'youtube' ? `/logos/youtube.svg` : '?'
+                link.type === 'youtube' ? '/logos/youtube.svg' :
+                  link.type === 'website' ? '/logos/globe.svg' :
+                    link.type === 'buy-tg' ? '/logos/telegram.svg' :
+                      link.type === 'source-github' ? '/logos/github.svg' : '?'
 " alt="">
                 <p class="font-[450]">{{
-                    link.type === "youtube" ? 'Видео' : '?'
+                    link.type === "youtube" ? 'Видео' :
+                        link.type === "website" ? 'Сайт' :
+                            link.type === "buy-tg" ? 'Купить' :
+                                link.type === "source-github" ? 'Исходники' : '?'
                   }}</p>
               </a>
             </div>
