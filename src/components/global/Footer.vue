71b8a47e7a76d1651ee240d20b55192d3d9fd3e1
zaralX <61970128+zaralX@users.noreply.github.com>
2024-11-09 17:48:57 +0800

    footer mobile adapt fix

--- src/components/global/Footer.vue
+++ src/components/global/Footer.vue
@@ -16,15 +16,15 @@ const socials = [
 </script>
 
 <template>
-  <div class="h-16 border-t-2 border-border-main-light dark:border-border-main-dark flex justify-center items-center text-border-inside-light dark:text-border-inside-dark font-[450] text-lg">
+  <div class="h-12 md:h-16 border-t-2 border-border-main-light dark:border-border-main-dark flex justify-center items-center text-border-inside-light dark:text-border-inside-dark font-[450] text-lg">
     <div class="px-4 h-full hidden md:flex items-center">find me in:</div>
 
     <nav class="relative grow border-x-2 border-border-main-light dark:border-border-main-dark h-full flex">
-      <a v-for="social in socials" :href="social.link" class="w-16 h-full flex items-center justify-center text-3xl gap-2 border-r-2 border-border-main-light dark:border-border-main-dark">
+      <a v-for="social in socials" :href="social.link" class="w-12 md:w-16 h-full flex items-center justify-center text-xl md:text-3xl gap-2 border-r-2 border-border-main-light dark:border-border-main-dark">
         <i class="pi" :class="social.icon"></i>
       </a>
     </nav>
 
-    <a href="https://github.com/zaralx" class="px-4 h-full flex items-center gap-2">@zaralx <i class="pi pi-github"></i></a>
+    <a href="https://github.com/zaralx" class="px-4 h-full flex items-center gap-2 text-sm md:text-base">@zaralx <i class="pi pi-github"></i></a>
   </div>
 </template>
