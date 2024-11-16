b8567c341ab65c8d438f64400d58e820fca25618
zaralX <61970128+zaralX@users.noreply.github.com>
2024-11-16 22:28:36 +0800

    login loading. for what? idk.

---
+++ src/components/global/Input.vue
@@ -0,0 +1,20 @@
+<script setup>
+const model = defineModel()
+
+const props = defineProps({
+  placeholder: { type: String, default: "Введите.." },
+  name: { type: String, default: "Имя" },
+})
+</script>
+
+<template>
+  <div>
+    <p class="text-sm text-neutral-500">{{ props.name }}</p>
+    <input
+        class="w-full bg-transparent border-2 rounded-lg px-2 py-1 border-neutral-700 outline-0 focus:animate-spin-ring"
+        :placeholder="props.placeholder"
+        type="text"
+        model-value="model"
+    />
+  </div>
+</template>
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,11 +1,169 @@
 <script setup>
+import { ref, onMounted } from 'vue';
 
+const firstLoadTexts = [
+  [
+    {
+      text: "zaralX Inc. [Version 1.2024.42]",
+      time: 500
+    },
+    {
+      text: "Making tea.. /",
+      time: 300,
+      next: {
+        text: "Making tea.. -",
+        time: 350,
+        next: {
+          text: "Making tea.. \\",
+          time: 250,
+          next: {
+            text: "Making tea.. |",
+            time: 400,
+            next: {
+              text: "Making tea.. ✔",
+              time: 300
+            }
+          },
+        }
+      }
+    },
+    {
+      text: "Making sandwiches.. /",
+      time: 300,
+      next: {
+        text: "Making sandwiches.. -",
+        time: 350,
+        next: {
+          text: "Making sandwiches.. \\",
+          time: 250,
+          next: {
+            text: "Making sandwiches.. |",
+            time: 400,
+            next: {
+              text: "Making sandwiches.. /",
+              time: 300,
+              next: {
+                text: "Making sandwiches.. -",
+                time: 300,
+                next: {
+                  text: "Making sandwiches.. \\",
+                  time: 350,
+                  next: {
+                    text: "Making sandwiches.. ✔",
+                    time: 400,
+                  },
+                }
+              }
+            }
+          },
+        }
+      }
+    },
+    {
+      text: "Turning on the computer.. /",
+      time: 400,
+      next: {
+        text: "Turning on the computer.. -",
+        time: 250,
+        next: {
+          text: "Turning on the computer.. \\",
+          time: 350,
+          next: {
+            text: "Turning on the computer.. |",
+            time: 300,
+            next: {
+              text: "Turning on the computer.. /",
+              time: 200,
+              next: {
+                text: "Turning on the computer.. -",
+                time: 400,
+                next: {
+                  text: "Turning on the computer.. \\",
+                  time: 250,
+                  next: {
+                    text: "Turning on the computer.. ✔",
+                    time: 600,
+                  },
+                }
+              }
+            }
+          },
+        }
+      }
+    },
+    {
+      text: "",
+      time: 100
+    },
+    {
+      text: "--<[ TASK FINISHED ]>--",
+      time: 400
+    },
+  ]
+];
+
+const currentText = ref('');
+const currentIndex = ref(0);
+
+function processTexts(textList) {
+  let index = 0;
+
+  function showNextText(textItem, beforeText = "") {
+    currentText.value = beforeText + textItem.text;
+
+    // Ждем указанный time
+    setTimeout(() => {
+      if (textItem.next) {
+        // Если есть next, продолжаем показывать следующий текст
+        showNextText(textItem.next, beforeText);
+      } else {
+        // Если next нет, переходим к следующему элементу списка
+        index++;
+        if (index < textList.length) {
+          showNextText(textList[index], currentText.value + "<br>");
+        } else {
+          // Если элементы закончились, начинаем снова с первого массива
+          currentIndex.value++;
+          if (currentIndex.value < firstLoadTexts.length) {
+            processTexts(firstLoadTexts[currentIndex.value]);
+          }
+        }
+      }
+    }, textItem.time);
+  }
+
+  // Запускаем первый элемент
+  if (textList.length > 0) {
+    showNextText(textList[0]);
+  }
+}
+
+onMounted(() => {
+  processTexts(firstLoadTexts[currentIndex.value]);
+});
 </script>
 
 <template>
+  <div class="flex-1 w-full h-full flex justify-center items-center">
+    <div class="bg-bg-second-dark rounded-lg md:w-96 h-64 shadow-lg relative">
+      <i class="absolute pi pi-circle-fill text-white blur-[90px] -z-10 text-[6rem] animate-[pulse_10s_ease-in-out_infinite]"></i>
+      <i class="absolute pi pi-circle-fill text-white blur-[70px] -z-10 text-[4rem] -bottom-2 -right-2 animate-[pulse_8s_ease-in-out_infinite]"></i>
 
+      <div class="bg-bg-third-dark py-1 px-2 rounded-t-lg flex items-center relative">
+        <div class="absolute left-2 text-[0.6rem] flex gap-2 items-center">
+          <i class="pi pi-circle-fill text-red-500"></i>
+          <i class="pi pi-circle-fill text-yellow-500"></i>
+          <i class="pi pi-circle-fill text-green-500"></i>
+        </div>
+        <div class="w-full text-center text-neutral-300">Вход в аккаунт</div>
+      </div>
+
+      <div class="p-2 h-full bg-neutral-950 rounded-b-lg" v-html="currentText">
+
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-
 </style>
