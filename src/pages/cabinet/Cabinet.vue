a030bf27c93c7a5a4c1e1887c2560c6bff1666b1
zaralX <61970128+zaralX@users.noreply.github.com>
2024-11-17 17:53:01 +0800

    connect buttons

--- src/pages/cabinet/Cabinet.vue
+++ src/pages/cabinet/Cabinet.vue
@@ -1,6 +1,7 @@
 <script setup>
 
 import store from "../../providers/store/index.js";
+import {config} from "../../providers/config/config.js";
 </script>
 
 <template>
@@ -12,6 +13,15 @@ import store from "../../providers/store/index.js";
   <button @click="store().Delete()" class="bg-red-600 hover:bg-red-700 transition-all duration-100 py-2 rounded-lg max-w-72 flex justify-start items-center px-3">
     <i class="pi pi-sign-out text-lg"></i>
     <span class="text-center w-full">Удалить аккаунт</span>
+  </button>
+  Привязка аккаунтов
+  <button :disabled="store()?.User?.discordId" class="bg-violet-700 disabled:opacity-75 enabled:hover:bg-violet-800 transition-all duration-100 py-2 rounded-lg flex max-w-72 justify-start items-center px-3">
+    <i class="pi pi-discord text-lg"></i>
+    <span class="text-center w-full">Привязать Discord</span>
+  </button>
+  <button :disabled="store()?.User?.telegramId" class="bg-blue-600 disabled:opacity-75 enabled:hover:bg-blue-700 transition-all duration-100 py-2 rounded-lg max-w-72 flex justify-start items-center px-3">
+    <i class="pi pi-telegram text-lg"></i>
+    <span class="text-center w-full">Привязать Telegram</span>
   </button>
 </template>
 
