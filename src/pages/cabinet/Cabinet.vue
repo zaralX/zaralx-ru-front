f124c2a062153dfd6e3677f2a1145e45a585fb5a
zaralX <61970128+zaralX@users.noreply.github.com>
2024-11-17 17:47:33 +0800

    logout and delete account

--- src/pages/cabinet/Cabinet.vue
+++ src/pages/cabinet/Cabinet.vue
@@ -5,6 +5,14 @@ import store from "../../providers/store/index.js";
 
 <template>
 {{store().User}}
+  <button @click="store().Logout()" class="bg-red-600 hover:bg-red-700 transition-all duration-100 py-2 rounded-lg max-w-72 flex justify-start items-center px-3">
+    <i class="pi pi-sign-out text-lg"></i>
+    <span class="text-center w-full">Выйти из аккаунта</span>
+  </button>
+  <button @click="store().Delete()" class="bg-red-600 hover:bg-red-700 transition-all duration-100 py-2 rounded-lg max-w-72 flex justify-start items-center px-3">
+    <i class="pi pi-sign-out text-lg"></i>
+    <span class="text-center w-full">Удалить аккаунт</span>
+  </button>
 </template>
 
 <style scoped>
