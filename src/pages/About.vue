860fa02b0a29a0fdc6647dfe85e334f3d007db4e
zaralX <61970128+zaralX@users.noreply.github.com>
2024-11-26 14:36:03 +0800

    profile

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -34,12 +34,25 @@ onUnmounted(() => {
 <!--</div>-->
   <div class="md:px-32 xl:px-64 py-8 grid grid-cols-1 lg:grid-cols-2 gap-4">
     <div class="flex flex-col gap-4">
+      <CategoryAbout icon="pi-address-book" title="profile.md">
+        <div class="flex gap-4">
+          <div class="w-24 aspect-square">
+            <div class="group/avatar w-24 h-24">
+              <img class="transition-all duration-500 group-hover/avatar:rounded-[50%] absolute w-24 aspect-square rounded-lg shadow-lg blur z-0" src="/img/profile.jpg" alt="profile">
+              <img class="transition-all duration-500 group-hover/avatar:rounded-[50%] w-24 aspect-square rounded-lg shadow-lg absolute z-10" src="/img/profile.jpg" alt="profile">
+            </div>
+          </div>
+          <div>
+            <p class="font-onest font-semibold text-2xl text-neutral-200 relative">Привет! Я <span class="bg-gradient-to-r from-amber-400 via-orange-400 to-yellow-400 inline-block text-transparent bg-clip-text animate-gradient">@zaralx</span><i class="right-8 pi pi-circle-fill blur-2xl text-3xl absolute text-amber-600 animate-[pulse_4s_cubic-bezier(0.6,0.2,0.5,1)_infinite]"></i></p>
+          </div>
+        </div>
+      </CategoryAbout>
       <CategoryAbout icon="pi-clock" title="datetime.json">
         <div class="bg-neutral-950/20 rounded-md p-2">
           <p class="text-neutral-500">{</p>
           <p>    <span class="text-amber-400">"string"</span><span class="text-neutral-400">:</span> <span class="text-amber-400">"{{datetime.string}}"</span><span class="text-neutral-500">,</span></p>
           <p>    <span class="text-amber-400">"millis"</span><span class="text-neutral-400">:</span> <span class="text-orange-400">{{datetime.unix}}</span><span class="text-neutral-500">,</span></p>
-          <p>    <span class="text-amber-400">"utc"</span><span class="text-neutral-400">:</span> <span class="text-amber-400">"UTC+8"</span><span class="text-neutral-500">,</span></p>
+          <p>    <span class="text-amber-400">"utc"</span><span class="text-neutral-400">:</span> <span class="text-amber-400">"UTC+8"</span></p>
           <p class="text-neutral-500">}</p>
         </div>
       </CategoryAbout>
@@ -51,5 +64,22 @@ onUnmounted(() => {
 </template>
 
 <style scoped>
+.animate-gradient {
+  background-size: 300%;
+  -webkit-animation: animatedgradient 6s ease infinite alternate;
+  -moz-animation: animatedgradient 6s ease infinite alternate;
+  animation: animatedgradient 6s ease infinite alternate;
+}
 
+@keyframes animatedgradient {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
 </style>
