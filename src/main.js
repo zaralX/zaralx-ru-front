e10edf29a7df2b23ebec219a7d8218ccd2f1082e
zaralX <61970128+zaralX@users.noreply.github.com>
2024-12-07 04:23:32 +0800

    cookies

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './assets/index.css'
 import App from './App.vue'
 import {router} from "./providers/router/index.js";
 import {createPinia} from "pinia";
+import VueCookies from 'vue-cookies'
 
 import 'primeicons/primeicons.css'
 import 'devicon/devicon.min.css'
@@ -10,4 +11,5 @@ import 'devicon/devicon.min.css'
 createApp(App)
     .use(router)
     .use(createPinia())
+    .use(VueCookies, { expires: '365d'})
     .mount('#app')
