296e961497e2b0172565e6994b879a4b2179cf1c
zaralX <61970128+zaralX@users.noreply.github.com>
2024-12-08 00:06:57 +0800

    base64 tools

--- src/pages/tools/Tools.vue
+++ src/pages/tools/Tools.vue
@@ -1,9 +1,13 @@
 <script setup>
-
+import Base64decodeToolCompact from "../../components/tools/compacts/Base64decodeToolCompact.vue";
+import Base64encodeToolCompact from "../../components/tools/compacts/Base64encodeToolCompact.vue";
 </script>
 
 <template>
-
+<div class="w-full grid grid-cols-3 gap-4 py-4 md:px-16 lg:px-32">
+  <Base64encodeToolCompact />
+  <Base64decodeToolCompact />
+</div>
 </template>
 
 <style scoped>
