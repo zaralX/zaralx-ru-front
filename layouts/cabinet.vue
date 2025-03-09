e77da344209c92427d1b942224b4b11d0e65670f
zaralX <61970128+zaralX@users.noreply.github.com>
2025-03-10 01:01:43 +0800

    cabinet layout fix

--- layouts/cabinet.vue
+++ layouts/cabinet.vue
@@ -5,9 +5,9 @@ import Header from "~/components/global/Header.vue";
 </script>
 
 <template>
-  <div class="min-h-screen flex flex-col">
+  <div class="h-screen flex flex-col">
     <Header />
-    <main class="flex-grow flex flex-col h-full w-full">
+    <main class="flex-1 flex flex-col w-full max-h-[calc(100vh-108px)] md:max-h-[calc(100vh-128px)] overflow-hidden">
       <div class="flex flex-col lg:flex-row flex-1">
         <div class="bg-bg-main-dark w-full lg:min-w-64 lg:w-64 h-full flex flex-col p-2">
           <NuxtLink
