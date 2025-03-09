2aba463b83ae159309df280dfb35fdd50e909c40
zaralX <61970128+zaralX@users.noreply.github.com>
2025-03-10 00:37:43 +0800

    mobile nav fix

--- components/global/Header.vue
+++ components/global/Header.vue
@@ -93,24 +93,26 @@ onUnmounted(() => {
 
 <template>
   <div class="min-h-16 border-b-2 border-border-main-light dark:border-border-main-dark flex justify-center items-center text-border-inside-light dark:text-border-inside-dark font-[450] text-lg z-30 bg-bg-main-light dark:bg-bg-main-dark">
-    <div v-if="mobileMenu" class="fixed w-screen h-screen top-0 left-0 z-20">
-      <div :class="mobileMenu ? 'top-0' : '-top-full'" class="absolute w-screen h-screen bg-bg-second-light dark:bg-bg-second-dark transition-all duration-200 ease-out pt-16">
-        <NuxtLink @click="mobileMenu = false" to="/pages/login" class="flex items-center justify-center border-border-third-light dark:border-border-third-dark transition-all duration-300 border-b-2 border-border-main-light dark:border-border-main-dark py-2 text-border-inside-light dark:text-border-inside-dark text-2xl">
-          login($me)
-        </NuxtLink>
-        <div class="text-2xl flex flex-col text-border-inside-light dark:text-border-inside-dark">
-          <NuxtLink
-              v-for="link in links"
-              :to="link.to"
-              class="flex items-center justify-center border-border-third-light dark:border-border-third-dark transition-all duration-300 border-b-2 border-border-main-light dark:border-border-main-dark py-2"
-              :class="route.fullPath === link.to ? '!text-amber-500' : ''"
-              @click="mobileMenu = false"
-          >
-            {{ link.name }}
+    <transition name="top-hide">
+      <div v-if="mobileMenu" class="fixed w-screen h-screen top-16 left-0 z-20">
+        <div class="absolute w-screen h-screen bg-bg-second-light dark:bg-bg-second-dark">
+          <NuxtLink @click="mobileMenu = false" to="/login" class="flex items-center justify-center border-border-third-light dark:border-border-third-dark transition-all duration-300 border-b-2 border-border-main-light dark:border-border-main-dark py-2 text-border-inside-light dark:text-border-inside-dark text-2xl">
+            login($me)
           </NuxtLink>
+          <div class="text-2xl flex flex-col text-border-inside-light dark:text-border-inside-dark">
+            <NuxtLink
+                v-for="link in links"
+                :to="link.to"
+                class="flex items-center justify-center border-border-third-light dark:border-border-third-dark transition-all duration-300 border-b-2 border-border-main-light dark:border-border-main-dark py-2"
+                :class="route.fullPath === link.to ? '!text-amber-500' : ''"
+                @click="mobileMenu = false"
+            >
+              {{ link.name }}
+            </NuxtLink>
+          </div>
         </div>
       </div>
-    </div>
+    </transition>
     <div class="px-8 md:min-w-64 h-full flex items-center text-center">zaralx-website</div>
 
     <nav class="relative grow border-x-2 border-border-main-light dark:border-border-main-dark h-full lg:grid grid-cols-5 hidden">
@@ -144,3 +146,15 @@ onUnmounted(() => {
     </div>
   </div>
 </template>
+
+<style scoped>
+.top-hide-enter-active,
+.top-hide-leave-active {
+  transition: all 0.5s ease;
+}
+
+.top-hide-enter-from,
+.top-hide-leave-to {
+  left: 100%;
+}
+</style>
