2561f68542fb268af255d80a01a0b07feae065c2
zaralX <61970128+zaralX@users.noreply.github.com>
2025-05-20 16:09:26 +0800

    text color fix

--- app.vue
+++ app.vue
@@ -9,9 +9,9 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="text-black font-fira dark:text-white w-full min-h-[100vh]">
+  <div class="text-white font-fira w-full min-h-[100vh]">
 
-    <div class="fixed top-0 left-0 w-full h-[100vh] bg-bg-main-light dark:bg-bg-main-dark -z-10 transition-all"/>
+    <div class="fixed top-0 left-0 w-full h-[100vh] bg-bg-main-light -z-10 transition-all"/>
 
     <NuxtLayout>
       <NuxtPage />
