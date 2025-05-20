9897f666df2f61f839eb9d1452d70c3deb25fabc
zaralX <61970128+zaralX@users.noreply.github.com>
2025-05-20 18:27:47 +0800

    git activity

---
+++ pages/activity/git.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import ContributionGraph from "~/components/charts/ContributionGraph.vue";
+
+definePageMeta({
+  layout: 'empty',
+})
+</script>
+
+<template>
+<div>
+  <ContributionGraph />
+</div>
+</template>
+
+<style scoped>
+
+</style>
