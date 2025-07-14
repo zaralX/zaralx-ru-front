1205b8db40bf3338adb092ce09d01ae34c872877
zaralX <61970128+zaralX@users.noreply.github.com>
2025-07-15 02:24:15 +0800

    removed unused

--- pages/tools/index.vue
+++ pages/tools/index.vue
@@ -9,9 +9,6 @@ import ContributionGraph from "~/components/charts/ContributionGraph.vue";
 <div class="w-full grid grid-cols-3 gap-4 py-4 md:px-16 lg:px-32">
   <Base64encodeToolCompact />
   <Base64decodeToolCompact />
-  <div class="col-span-3">
-    <ContributionGraph />
-  </div>
 </div>
 </template>
 
