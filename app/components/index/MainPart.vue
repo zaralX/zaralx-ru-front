82b5a3007e89450a4c39165b31f2cbdf09e305ff
zaralX <looppersmail@gmail.com>
2025-11-12 20:49:44 +0800

    mini projects in progress

--- app/components/index/MainPart.vue
+++ app/components/index/MainPart.vue
@@ -38,9 +38,27 @@ onMounted(() => {
         <PerspectiveCube />
       </div>
     </div>
-    <div class="absolute top-1/2 left-1/2 transform -translate-1/2 text-center">
+    <div class="absolute top-1/2 left-1/2 transform -translate-1/2">
       <p id="index-hello" class="text-stone-400">Привет, я:</p>
       <p id="index-title" class="text-5xl font-unbounded">zaralX</p>
+    </div>
+    <div class="absolute top-1/2 transform -translate-y-1/2">
+      <div class="w-[25vw]">
+        <IndexProjectWindow :z="3" translate-y="-25%" id="index-project-1" />
+        <IndexProjectWindow :z="2" translate-y="-50%" id="index-project-2" />
+        <IndexProjectWindow :z="1" translate-y="-75%" id="index-project-3" />
+        <div class="absolute aspect-video bg-stone-950 w-[25vw] z-50 h-8 top-48 blur-sm"></div>
+        <div class="absolute aspect-video bg-stone-950 w-[25vw] z-50 h-4 top-52"></div>
+      </div>
+    </div>
+    <div class="absolute top-1/2 right-0 transform -translate-y-1/2">
+      <div>
+        <IndexProjectWindow :z="3" translate-y="-25%" x-full id="index-project-4" />
+        <IndexProjectWindow :z="2" translate-y="-50%" x-full id="index-project-5" />
+        <IndexProjectWindow :z="1" translate-y="-75%" x-full id="index-project-6" />
+        <div class="absolute aspect-video bg-stone-950 w-[25vw] z-50 h-8 top-48 blur-sm -translate-x-full"></div>
+        <div class="absolute aspect-video bg-stone-950 w-[25vw] z-50 h-4 top-52 -translate-x-full"></div>
+      </div>
     </div>
   </div>
 </template>
---
+++ app/components/index/ProjectWindow.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import type {PrimitiveProps} from "reka-ui";
+import {type HTMLAttributes, onMounted} from "vue";
+import {animate, createDraggable, random, splitText, stagger} from "animejs";
+
+interface Props extends PrimitiveProps {
+  class?: HTMLAttributes["class"]
+  z?: number
+  translateY?: string
+  xFull?: boolean
+  id: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  z: 1,
+  translateY: "50%",
+  xFull: false
+})
+
+onMounted(() => {
+  animate('#' + props.id, {
+    // translateX: () => [random(10, 20) * random(10, 20), 0],
+    opacity: [0, 1],
+    ease: 'easeIn',
+    duration: 750,
+  });
+
+
+})
+</script>
+
+<template>
+  <div>
+    <div :id="id" :class="{ '-translate-x-full': xFull }" class="opacity-0 absolute aspect-video bg-stone-900 p-1 rounded-md w-[25vw] space-y-1 shadow-md border border-white/10 group hover:-translate-y-2 duration-200" :style="{ transform: `translateY(${translateY})`, zIndex: z }">
+      <div class="flex items-center text-stone-400 px-1">
+        <div class="flex items-center gap-1 flex-1">
+          <Icon name="lucide:folder" />
+          <p class="text-sm">Проект такой то</p>
+        </div>
+        <div class="flex items-center">
+          <Icon name="lucide:x" />
+        </div>
+      </div>
+      <img class="h-full rounded-sm" src="/img/fillers/1.png" alt="">
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
