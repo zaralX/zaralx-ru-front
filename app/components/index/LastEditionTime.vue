1c9c2b803c74d1b1f7f2b011685d9c7a0a000f42
zaralX <looppersmail@gmail.com>
2025-11-25 23:19:16 +0800

    last edition time

---
+++ app/components/index/LastEditionTime.vue
@@ -0,0 +1,9 @@
+<script setup>
+import AnimatedCryptoText from "~/components/common/AnimatedCryptoText.vue";
+
+const { data } = await useFetch("/api/last-commit");
+</script>
+
+<template>
+  Последнее изменение: <AnimatedCryptoText v-if="data.date" v-model="data.date" />
+</template>
